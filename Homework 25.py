c3cd285c52bb99e42aac67af5291ca4e74ef61a0
KKwins04 <arvind_kavin_karthik@students.edu.sg>
2026-02-18 16:31:31 +0800

    Homework 25 commit

---
+++ Homework 25.py
@@ -0,0 +1,11 @@
+Set1 = {"blue", "green"} 
+Set2 = {"blue", "yellow"}
+
+Set3 = Set1.symmetric_difference(Set2)
+print(Set3)
+
+Set1 = {1,2,3,4,5}
+Set2 = {1,5,6,7,8,9}
+
+Set3 = Set1.symmetric_difference(Set2)
+print(Set3)
