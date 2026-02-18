fb065cd2c33cffa7f804b643af548c9af435f6e0
KKwins04 <arvind_kavin_karthik@students.edu.sg>
2026-02-18 16:24:30 +0800

    Exit commit

---
+++ Exit.py
@@ -0,0 +1,11 @@
+# Python program to demonstrate exit()
+
+for i in range(10):
+
+    # If value of i becomes 5, the program is forced to exit
+    if i == 6:
+
+        # Print the exit message
+        print(exit)
+        exit()
+    print(i)
